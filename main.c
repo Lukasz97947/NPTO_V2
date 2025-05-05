dd522641c8c452195b8e62c10891c90473f73a36
Lukasz97947 <91849@student.pb.edu.pl>
2025-05-05 19:27:40 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    // Pêtla wypisuj¹ca komunikat 20 razy
-    for (int i = 0; i < 20; i++) {
+    // PÃªtla wypisujÂ¹ca komunikat 1 000 000 razy
+    for (int i = 0; i < 1000000; i++) {
         printf("Witamy w Npto\n");
     }
     return 0;
