206d99dfa2f73087c82695d668280a0d11428b71
Lukasz97947 <91849@student.pb.edu.pl>
2025-05-06 09:05:00 +0200

    Delete main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Pêtla wypisuj¹ca komunikat 1 000 000 razy
-    for (int i = 0; i < 1000000; i++) {
-        printf("Witamy w Npto\n");
-    }
-    return 0;
-}
